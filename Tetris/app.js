f2202d557139aad381db685ab6a9367cb6b7d530
jbrown7407 <54042125+jbrown7407@users.noreply.github.com>
2020-08-15 14:23:06 -0400

    x

--- Tetris/app.js
+++ Tetris/app.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded' () => {
   //Code goes here. fires when HTML has been loaded and passed
   //20 tall, 10 in height
+//document refers to HTML document
+
 
 
 console.log("worklog");
 })
+// 14:00
