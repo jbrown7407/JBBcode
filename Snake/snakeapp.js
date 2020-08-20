b7928899aa5116498fadec90bb449fd47bdd0735
jbrown7407 <54042125+jbrown7407@users.noreply.github.com>
2020-08-19 20:26:38 -0400

    still have a bug
    still have a bug. event listener issue?

--- Snake/snakeapp.js
+++ Snake/snakeapp.js
@@ -14,11 +14,63 @@ document.addEventListener('DOMContentLoaded', () => {
   let interval = 0
 
 //  start restart
+function startGame() {
+  currentSnake.forEach(index => squares[index].classList.remove('snake')) // each index of current snake array
+  squares[appleIndex].classList.remove('apple')
+  clearInterval(interval)
+  score = 0
+  //randomzie apple
+  direction = 1
+  scoreDisplay.innerText = score
+  intervalTime = 1000
+  currentSnake = [2, 1, 0]
+  currentIndex = 0
+  currentSnake.forEach(index => squares[index].classList.add('snake'))
+  interval = setInterval(moveOutcomes, intervalTime)
+}
+
+//  function for outcomes
+function moveOutcomes() {}
+//  function snake hits border/self
+  if(
+    (currentSnake[0] + width >= (width * width) && direction === width) || //if snake hits the bottom
+    (currentSnake[0] % width === width -1 && direction === 1) || //if snake hits right wall
+    (currentSnake[0] % width === 0 && direction === -1) || // if snake hits left wall
+    (currentSnake[0] - width < 0 && direction === -width) //  if snake hits top
+    squares[currentSnake[0] + direction].classList.contains('snake') // snake overlaps itself
+  ) {
+    return clearInterval(interval) //clear interval if any of above occur.
+  }
+
+  const tail = currentsnake.pop() // removes last of the array and shows it
+  squares[tail].classList.remove('snake') //removes class of snake from the tail pieces
+  currentSnake.unshift(currentSnake[0] + direction) //direction for the head
+//  function snake gets apple
+if(squares[currentSnake[0]].classList.contains('apple') } {
+  squares[currentSnake[0]].classList.remove('apple')
+  squares[tail].classList.add('snake')]
+  currentSnake.push(tail)
+  randomApple()
+  score++
+  scoreDisplay.textContent = score
+  clearInterval(interval)
+  intervalTime = intervalTIme * speed
+  interval = setInteval(moveOutcomes, intervalTime)
+}
+//generate new apple after it is eaten
+function ranomApple() {
+  do{
+    appleIndex = Math.floor(Math.random() * squares.length)
+  } while(squares[appleIndex].classList.contains('snake'))
+  squares[appleIndex].classList.add('apple')
+}// EXTRA?
+}
 
 
+squares[currentSnake[0]}.classList.add('snake')
 
   function control (e) {
-    squares[currentIndex].classList.remove('snake') // remove snake class
+    squares[currentIndex].classList.remove('snake') // remove snake class from being left behind
     if(e.keycode === 39) {
       direction = 1 //   if we press right
     } else if (e.keycode === 38) {
@@ -26,10 +78,10 @@ document.addEventListener('DOMContentLoaded', () => {
     } else if (e.keycode === 37) {
       direction = -1//  left
     } else if (e.keycode === 40) {
-      direction = +width//  down
+      direction = +width//  down, snake appears 10 divs away
     }
   }
 
-  document.addEventListener('keyup', control)
-
+document.addEventListener('keyup', control)
+startBtn.addEventListener('click', startGame)
 })
