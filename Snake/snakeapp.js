e4e50f9098819cca20945258313d11d4cba10e8d
jbrown7407 <54042125+jbrown7407@users.noreply.github.com>
2020-08-19 20:42:31 -0400

    x
    x

--- Snake/snakeapp.js
+++ Snake/snakeapp.js
@@ -13,13 +13,13 @@ document.addEventListener('DOMContentLoaded', () => {
   let speed = .9
   let interval = 0
 
-//  start restart
+//  start/restart
 function startGame() {
   currentSnake.forEach(index => squares[index].classList.remove('snake')) // each index of current snake array
   squares[appleIndex].classList.remove('apple')
   clearInterval(interval)
   score = 0
-  //randomzie apple
+randomApple()
   direction = 1
   scoreDisplay.innerText = score
   intervalTime = 1000
@@ -36,7 +36,7 @@ function moveOutcomes() {}
     (currentSnake[0] + width >= (width * width) && direction === width) || //if snake hits the bottom
     (currentSnake[0] % width === width -1 && direction === 1) || //if snake hits right wall
     (currentSnake[0] % width === 0 && direction === -1) || // if snake hits left wall
-    (currentSnake[0] - width < 0 && direction === -width) //  if snake hits top
+    (currentSnake[0] - width < 0 && direction === -width) || //  if snake hits top
     squares[currentSnake[0] + direction].classList.contains('snake') // snake overlaps itself
   ) {
     return clearInterval(interval) //clear interval if any of above occur.
@@ -44,11 +44,11 @@ function moveOutcomes() {}
 
   const tail = currentsnake.pop() // removes last of the array and shows it
   squares[tail].classList.remove('snake') //removes class of snake from the tail pieces
-  currentSnake.unshift(currentSnake[0] + direction) //direction for the head
+  currentSnake.unshift(currentSnake[0] + direction) //direction for the head. .unshift
 //  function snake gets apple
-if(squares[currentSnake[0]].classList.contains('apple') } {
+if(squares[currentSnake[0]].classList.contains('apple')) {
   squares[currentSnake[0]].classList.remove('apple')
-  squares[tail].classList.add('snake')]
+  squares[tail].classList.add('snake')
   currentSnake.push(tail)
   randomApple()
   score++
@@ -56,21 +56,20 @@ if(squares[currentSnake[0]].classList.contains('apple') } {
   clearInterval(interval)
   intervalTime = intervalTIme * speed
   interval = setInteval(moveOutcomes, intervalTime)
+  }
+squares[currentSnake[0]].classList.add('snake')
 }
 //generate new apple after it is eaten
-function ranomApple() {
+function randomApple() { //DO WHILE
   do{
     appleIndex = Math.floor(Math.random() * squares.length)
   } while(squares[appleIndex].classList.contains('snake'))
   squares[appleIndex].classList.add('apple')
-}// EXTRA?
 }
-
-
-squares[currentSnake[0]}.classList.add('snake')
 
   function control (e) {
     squares[currentIndex].classList.remove('snake') // remove snake class from being left behind
+
     if(e.keycode === 39) {
       direction = 1 //   if we press right
     } else if (e.keycode === 38) {
