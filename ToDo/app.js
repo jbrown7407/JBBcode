e4fa37afdb3163ff0292eeb675652d0805ec8f30
jbrown7407 <54042125+jbrown7407@users.noreply.github.com>
2020-09-11 19:14:31 -0400

    x

--- ToDo/app.js
+++ ToDo/app.js
@@ -155,9 +155,20 @@ list.addEventListener("click", function(event){
 
 
 
+/* TOGGLE FUNCTION
+let image = document.querySelector("#kitten")
 
+image.onclick = toggleClass;
 
+function toggleClass(){
+ if(image.className == 'image'){
+     image.className = ''
+ } else {
+     image.className = 'image'
+ }
+}
 
+*/
 
 
 
