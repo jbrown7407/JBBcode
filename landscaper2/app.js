15a1ddcb660782259cd2bbc55a3d6c488a317ecb
jbrown7407 <jbrown7407@gmail.com>
2020-09-28 16:00:18 -0400

    IDE draft
    x

--- landscaper2/app.js
+++ landscaper2/app.js
@@ -1,7 +1,7 @@
 let clicks = 0
 let cash = 0
 let level
-let rank = "Grasshopper"
+let rank = ""
 let highScore = 0
 let score = 0
 
@@ -9,59 +9,38 @@ let upgrade = document.getElementById ("upgradeTool")
 
 let player = {
     powerLevel: 1
+ 
 }
 
-let tool = ["Teeth", "Safety scissors", "Snipper", "Weedwacker", "Shears", "Katana", "Lawn-Saber", "Attack Chopper", "Mow-Ops Team"]
-let equipped = tool[0]
+let tool = ["Teeth", "Metal Teeth", "Pluck", "Sticks", "Safe Scissors", "Scissors", "Exacto", "Tiny Sword", "Snipper", "Weedwacker", "Shears", "Katana", "Lawn-Saber", "Attack Chopper", "Mow-Ops Team", "Nukes"
+, "Planet Buster"]
+
 let power = player.powerLevel 
+let equipped ="Teeth"
 
-//shop = ["teeth", "weapon2", "weapon3"]
-
-
-
+//FIX WEAPON SELECTION
 function upgradeTool() {
   
-     if (cash >= 10) {
+     if (cash >= 10 && upgrade < (tool.length-1)) {
         cash = cash - 10
         power = power + 1
+        upgrade = upgrade +1
+        equipped= tool[power-1]
         document.getElementById("clicks").innerHTML = clicks;  
         document.getElementById("cash").innerHTML = cash;
         document.getElementById("level").innerHTML = level;
-        document.getElementById("rank").innerHTML = rank;
         document.getElementById("highScore").innerHTML = highScore;
         document.getElementById("power").innerHTML = power;
-     }
-     else  {alert("You are too broke!")}; 
-
-
-     if (player.powerLevel = 1 && cash>=10){
-         equipped=tool[1]
-         document.getElementById("equipped").innerHTML = equipped;
-     }
-     else if (player.powerLevel = 2){
-        equipped=tool[2]
-        document.getElementById("equipped").innerHTML = equipped;}
-     else if (player.powerLevel = 3){
-        equipped=tool[3]
-        document.getElementById("equipped").innerHTML = equipped;}
-     else if (player.powerLevel = 4){
-        equipped=tool[3]}
-     else if (player.powerLevel = 5){
-        equipped=tool[4]}
-     else if (player.powerLevel = 6){
-        equipped=tool[5]}
-    else if (player.powerLevel = 7){
-        equipped=tool[6]}
-    else if (player.powerLevel = 8){
-        equipped=tool[7]}
-    else if (player.powerLevel = 9){
-        equipped=tool[8]}
+        document.getElementById("equipped").innerHTML = equipped;
+    }
+    else if (upgrade >= (tool.length-1))  {
+       
+        document.getElementById("equipped").innerHTML = "Planet Buster";}
     else {
-        equipped=tool[9]}
+    alert("You are too broke!")
+    }
+    
 }
-
-
-
 
 function onClick() { //Called only when MOW BUTTON is pressed
    
@@ -83,21 +62,27 @@ document.getElementById("power").innerHTML = power;
 if (clicks<2) {
 
       alert("YOUR JOURNEY BEGINS, YOUNG GRASSHOPPER....")
-      document.getElementById('clicks').style.backgroundColor = "lightgreen";}
+      document.getElementById('clicks').style.backgroundColor = "lightgreen";
+      document.getElementById("rank").innerHTML = "Grasshopper";}
               else if (clicks==2) {
-                  document.getElementById('clicks').style.backgroundColor = "orange";}
+                  document.getElementById('clicks').style.backgroundColor = "orange";
+                  document.getElementById("rank").innerHTML = "Grasshopper";}
               else if (clicks==3) {
                 alert("THREE LAWNS...!")
                 document.getElementById('clicks').style.Color = "black";
-                document.getElementById('clicks').style.backgroundColor = "lightyellow";}
+                document.getElementById('clicks').style.backgroundColor = "lightyellow";
+                document.getElementById("rank").innerHTML = "Grasshopper";}
               else if (clicks==4) {
                 document.getElementById('clicks').style.Color = "black";
-                document.getElementById('clicks').style.backgroundColor = "yellow";}
+                document.getElementById('clicks').style.backgroundColor = "yellow";
+                document.getElementById("rank").innerHTML = "Grasshopper";}
               else if (clicks==5) {
                   alert("FIVE LAWNS... You are an entrepeneur!")
-                  document.getElementById('clicks').style.Color = "green";}
+                  document.getElementById('clicks').style.Color = "green";
+                  document.getElementById("rank").innerHTML = "Grasshopper";}
               else if (clicks >= 6 && clicks <=9) {
-              document.getElementById('clicks').style.color = "darkgreen";}
+              document.getElementById('clicks').style.color = "darkgreen";
+              document.getElementById("rank").innerHTML = "Grasshopper";}
               else if (clicks ==10) {
                   alert("TEN LAWNS! You should probably invest in protecting your teeth.")
                   document.getElementById("rank").innerHTML = "Peon";}
@@ -110,16 +95,19 @@ if (clicks<2) {
               else if (clicks >= 40 && clicks <50 ) {
                   document.getElementById("rank").innerHTML = "Baby Tycoon";}
               else if (clicks == 50) {
-                  alert("You have potential...")
-                  document.getElementById("rank").innerHTML = "Lawntrepeneur";}
+                  alert("You have potential...")}
               else if (clicks > 50 && clicks <60 ) {
-                  document.getElementById("rank").innerHTML = "Lawntrepeneur";}
+                  document.getElementById("rank").innerHTML = "Grass Barber";}
               else if (clicks >= 60 && clicks <70 ) {
                     document.getElementById("rank").innerHTML = "Lawn Doctor";}
               else if (clicks >= 70 && clicks <80 ) {
-                    document.getElementById("rank").innerHTML = "Trim Trooper";}         
+                    document.getElementById("rank").innerHTML = "Trim Trooper";}   
+             else if (clicks >= 80 && clicks <90 ) {
+                    document.getElementById("rank").innerHTML = "Landscape Lion";} 
+             else if (clicks >= 90 && clicks <100 ) {
+                     document.getElementById("rank").innerHTML = "Grass Deity";}             
               else if (clicks >= 100 && clicks <=499) { 
-                  document.getElementById("rank").innerHTML = "Cut Commander";}
+                  document.getElementById("rank").innerHTML = "Crop Commander";}
               else if (clicks >= 500 && clicks <=1000) {
                   document.getElementById("rank").innerHTML = "Snip Sensei";}
               else if (clicks >= 1001 && clicks <=9999) {
@@ -131,6 +119,3 @@ if (clicks<2) {
       alert("EDGECASE for RANK")
     }
 }
-  //  document.getElementById("mow").addEventListener("mow", onClick());
-
-//document.getElementById("upgrade").onclick = function() {upgradeTool()};
