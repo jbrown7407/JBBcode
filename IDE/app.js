fdd8fc982fa23e48a865cb8f0e16193cd3da4fea
jbrown7407 <jbrown7407@gmail.com>
2020-10-03 18:31:45 -0400

    IDE full screen mode, to do list work
    x

--- IDE/app.js
+++ IDE/app.js
@@ -21,6 +21,9 @@ let function renameFile() {
 console.log("renameFile called")
 }
 */
+
+
+
 function run() {
   let htmlCode = document.querySelector(".editor #html").value; //store editor content as variables
   let cssCode = "<style>"+document.querySelector(".editor #css").value+"</style>";
@@ -71,44 +74,51 @@ function copyThisHTML() {  //COPIES HTML TO CLIPBOARD
   document.querySelector(".editor #html").addEventListener("keyup",run); //update html in editor each press by calling run()
   document.querySelector(".editor #css").addEventListener("keyup",run);  //update css in editor each press by calling run()
   //document.querySelector(".editor #js").addEventListener("keyup",run);   //update js only onClick(), this updates every keystroke (replaced with button)
+  
+  
+  
+  
+  $(function() {  //JQUERY
+
+
+  const $phase2 = document.getElementsByClassName("phase2")
+
+  const $phase1 = document.getElementsByClassName("phase1")
+  $("#btn2").hide()
+ 
+   $("#btn2").click(function restore(){ //TITLE SCREEN
+ 
+    $(".phase1").fadeIn(700).show();
+    $("#btn1").show()
+    $("#btn2").hide()
+    $(".phase2").show()
+
+    cl("Title Screen")
+       });
+let counterx = 1
+  $("#btn1").click(function fullScreen(){ //Full Screen
+        $(".phase3").hide()
+        $(".phase1").hide();
+        $(".phase4").hide()
+        $(".phase2").show();
+        $(".phase5").hide()
+        console.log("Full screen preview")
+  
+        counterx++
+        $("#btn1").hide()
+        $("#btn2").show()
+        
 
 
 
-////SAVE FILE CODE
-/*function saveFile() {
-const downloadToFile = (content, filename, contentType) => {
-  const a = document.createElement('a');
-  const file = new Blob([content], {type: contentType});
-  a.href= URL.createObjectURL(file);
-  a.download = filename;
-  a.click();
-    URL.revokeObjectURL(a.href);
-};
-document.querySelector('#btnSave').addEventListener('click', () => {
-  const textArea = document.querySelector('textarea');
-  downloadToFile(textArea.value, 'my-new-file.txt', 'text/plain');
-});
-}
-*/
+          // function fullScreen() {
+          //   $(".phase3").hide()
+          //   $(".phase1").hide();
+          //   $(".phase4").hide()
+          //   $(".phase2").fadeIn(700).show();
+          //   $(".phase5").hide()
+          //  }
 
+  
 
-
-
-
-/*
-  For custom clear and execute shortcuts Ctrl-Enter and Ctrl-I
- function kbd(e){
-  if (e.key === "i") html.clear();
-  if (e.key === "Enter") eval(editor.getValue());
-}
-
-window.addEventListener('keydown', function (e) {
-  if (e.key === "Control") window.addEventListener('keydown', kbd);
-});
-window.addEventListener('keyup', function (e) {
-  if(e.key === "Control") window.removeEventListener('keydown', kbd);
-})
-// For phones and devices without a control key
-$("[action]").get(0).addEventListener('click', function (){ eval(editor.getValue()); });
-$("[action]").get(1).addEventListener('click', function (){ html.clear(); });
-*/
+          })})
--- aStarters/script.js
+++ aStarters/script.js
@@ -1,6 +1,14 @@
-let cl = (value) => console.log(value);
-//this function lets you just type "cl" instead of having to type out console.log
+$(document).ready(function(){
+    let cl = (value) => console.log(value);
+    cl("Jquery Active")
+    
+    const $div = $('<div>')
+    $($div).addClass('title')
+    $('body').append($div)
+    $($div).text("This is the first div title")
+   // $($div).style.color("blue")
+//JQUERY HERE
 
-let function func {
-    console.log("DO STUFF")
-}
+
+   
+})
